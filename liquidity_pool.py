3ecf60d5447e3cdeb349726f235a312b1e6a6367
R <ryansereno@protonmail.com>
2021-11-21 12:25:55 -0800

    added script loop

--- liquidity_pool.py
+++ liquidity_pool.py
@@ -1,10 +1,10 @@
-token_A_quantity = 50000
-token_B_quantity = 50000
-pegged_value = 1
+#token_A_quantity = 50000
+#token_B_quantity = 50000
+#pegged_value = 1
 
-print("Token A quantity: ", token_A_quantity)
-print("Token B quantity: ", token_B_quantity)
-print(f'Pegged value: {pegged_value} USD')
+#print("Token A quantity: ", token_A_quantity)
+#print("Token B quantity: ", token_B_quantity)
+#print(f'Pegged value: {pegged_value} USD')
 def trade(A_trade_quantity, token_A_quantity, token_B_quantity):
     token_A_quantity += A_trade_quantity
     new_token_B_quantity_cache = 50000**2/ token_A_quantity
@@ -13,6 +13,12 @@ def trade(A_trade_quantity, token_A_quantity, token_B_quantity):
     token_A_value = token_A_quantity / 50000
     token_B_value = token_B_quantity / 50000
     print("Quantity of B tokens returned: ", B_trade_quantity)
-
-A_trade_quantity = float(input("\n Enter quantity of A tokens to be traded: "))
-trade(A_trade_quantity, token_A_quantity, token_B_quantity)
+while True:
+    token_A_quantity = 50000
+    token_B_quantity = 50000
+    pegged_value = 1
+    print("Token A quantity: ", token_A_quantity)
+    print("Token B quantity: ", token_B_quantity)
+    print(f'Pegged value: {pegged_value} USD')
+    A_trade_quantity = float(input("\n Enter quantity of A tokens to be traded: "))
+    trade(A_trade_quantity, token_A_quantity, token_B_quantity)
