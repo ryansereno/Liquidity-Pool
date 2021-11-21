a4877306c7d8caff376f73f98d6641cf357691d3
R <ryansereno@protonmail.com>
2021-11-21 12:50:47 -0800

    corrected asset value calculation

--- liquidity_pool.py
+++ liquidity_pool.py
@@ -9,9 +9,11 @@ def trade(A_trade_quantity):
     new_token_B_quantity_cache = 50000**2/ token_A_quantity
     B_trade_quantity = token_B_quantity - new_token_B_quantity_cache
     token_B_quantity = new_token_B_quantity_cache
-    token_A_value = token_A_quantity / 50000
-    token_B_value = token_B_quantity / 50000
+    token_A_value = 50000 / token_A_quantity
+    token_B_value = 50000 / token_B_quantity
     print("Quantity of B tokens returned: ", B_trade_quantity)
+    print("Token A value: ", token_A_value, "USD")
+    print("Token B value: ", token_B_value, "USD")
 
 while True:
     print("Token A quantity: ", token_A_quantity)
