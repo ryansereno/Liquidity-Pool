4a116d96587a2d1303ee52ac43a074c12e6a1901
R <ryansereno@protonmail.com>
2021-11-20 19:03:37 -0800

    inserted trade algorithm

--- liquidity_pool.py
+++ liquidity_pool.py
@@ -1,3 +1,13 @@
 token_A_quantity = 50000
 token_B_quantity = 50000
 pegged_value = 1
+
+print("Token A quantity: ")
+print("Token B quantity: ")
+def trade(A_trade_quantity):
+    token_A_quantity += A_trade_quantity
+    new_token_B_quantity_cache = 50000^2/ token_A_quantity
+    B_trade_quantity = token_B_quantity - new_token_B_quantity_cache
+    token_B_quantity = new_token_B_quantity_cache
+    token_A_value = token_A_quantity / 50000
+    token_B_value = token_B_quantity / 50000
