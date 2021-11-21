fedc376d6a253ae185dc520a766c58c13bafb67f
R <ryansereno@protonmail.com>
2021-11-21 07:19:02 -0800

    changed trade() function

--- liquidity_pool.py
+++ liquidity_pool.py
@@ -2,12 +2,17 @@ token_A_quantity = 50000
 token_B_quantity = 50000
 pegged_value = 1
 
-print("Token A quantity: ")
-print("Token B quantity: ")
-def trade(A_trade_quantity):
+print("Token A quantity: ", token_A_quantity)
+print("Token B quantity: ", token_B_quantity)
+print(f'Pegged value: {pegged_value} USD')
+def trade(A_trade_quantity, token_A_quantity, token_B_quantity):
     token_A_quantity += A_trade_quantity
-    new_token_B_quantity_cache = 50000^2/ token_A_quantity
+    new_token_B_quantity_cache = 50000**2/ token_A_quantity
     B_trade_quantity = token_B_quantity - new_token_B_quantity_cache
     token_B_quantity = new_token_B_quantity_cache
     token_A_value = token_A_quantity / 50000
     token_B_value = token_B_quantity / 50000
+    print("Quantity of B tokens returned: ", B_trade_quantity)
+
+A_trade_quantity = float(input("\n Enter quantity of A tokens to be traded: "))
+trade(A_trade_quantity, token_A_quantity, token_B_quantity)
